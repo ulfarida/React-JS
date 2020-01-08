8604d00ab3cfc16843a7cd0207dd28a29a38e802
ulfarida <ulfah@alterra.id>
2020-01-08 21:06:49 +0700

    ready to deploy

---
+++ build/precache-manifest.470177d811af2f7ebe10fe5287c3a5db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8b5a87593eb5274c5a9a0baf226ca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e6d029ea2f33af84a6",
+    "url": "/static/css/main.72f727b1.chunk.css"
+  },
+  {
+    "revision": "d120aeb7560be3760a6e",
+    "url": "/static/js/2.9f268019.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.9f268019.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4e6d029ea2f33af84a6",
+    "url": "/static/js/main.8a15cb91.chunk.js"
+  },
+  {
+    "revision": "751a46a96cea0304b0dd",
+    "url": "/static/js/runtime-main.4e52f170.js"
+  },
+  {
+    "revision": "3a2ea042c4bc701101fa1eeac6c5afd8",
+    "url": "/static/media/dislike.3a2ea042.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "14345b3a71065f96b74f2fa2ad946c72",
+    "url": "/static/media/love.14345b3a.png"
+  },
+  {
+    "revision": "52e4216b354e0b9bf24182b25dfc9fa5",
+    "url": "/static/media/search.52e4216b.png"
+  }
+]);
